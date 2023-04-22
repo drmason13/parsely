e51ee68232e7c2661ad5501a9846dfc603627216
drmason13 <drmason13@users.noreply.github.com>
2023-04-22 19:03:01 +0100

    remove E: Error bound on Map Parse impl

--- src/combinator/map.rs
+++ src/combinator/map.rs
@@ -18,7 +18,6 @@ where
 impl<L: Lex, F, O, E> Parse for Map<L, F>
 where
     F: Fn(&str) -> Result<O, E>,
-    E: std::error::Error,
 {
     type Output = O;
 
@@ -34,7 +33,6 @@ impl<L, F, O, E> fmt::Debug for Map<L, F>
 where
     L: fmt::Debug,
     F: Fn(&str) -> Result<O, E>,
-    E: std::error::Error,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "Map<{:?} -> {}>", self.lexer, type_name::<O>())
